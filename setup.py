5c008ff11dc0d5d788a85c0287befcaf8df1f5ac
Sergio Pascual <sergiopr@fis.ucm.es>
2020-10-07 16:36:53 +0200

    Improve setup with setup.cfg

--- setup.py
+++ setup.py
@@ -19,9 +19,23 @@ setup(
         'fridadrp.instrument.configs': ['*.json']
     },
     install_requires=[
-        'setuptools',
-        'numina >= 0.16',
+        'setuptools>=36.2.1',
+        'six',
+        'numpy',
+        'matplotlib',
+        'astropy >= 2',
+        'scipy',
+        'numina >= 0.22',
+        'enum34;python_version<"3.4"',
+        'contextlib2;python_version<"3.5"',
     ],
+    extras_require={
+        'test': [
+            'pytest<3.7; python_version<"3"',
+            'pytest>=6; python_version>="3"',
+            'pytest-remotedata'
+            ]
+    },
     entry_points={
         'numina.pipeline.1': [
             'FRIDA = fridadrp.loader:drp_load',
@@ -29,9 +43,10 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         'Development Status :: 3 - Alpha',
         "Environment :: Console",
         "Intended Audience :: Science/Research",
