c7ad2c3d3f7b218b15f1261c79618e3ca5922f48
Sergio Pascual <sergiopr@fis.ucm.es>
2020-10-07 17:01:00 +0200

    Fix instrument json test

--- fridadrp/instrument/tests/test_configs.py
+++ fridadrp/instrument/tests/test_configs.py
@@ -1,19 +1,35 @@
 import pytest
-import numina.core.pipeline
+import astropy.io.fits as fits
+import numina.instrument.generic
+import numina.core
+from fridadrp.loader import drp_load
 
-import fridadrp.loader
+
+def create_simple_frame():
+    hdr = {'INSTRUME': 'FRIDA', 'INSCONF': 'c7f94f7d-1f57-4644-86d6-7004a2506680'}
+    hdu = fits.PrimaryHDU(data=[[1]])
+    for k, v in hdr.items():
+        hdu.header[k] = v
+    hdulist = fits.HDUList([hdu])
+    frame = numina.core.DataFrame(frame=hdulist)
+    return frame
 
 
-@pytest.mark.parametrize('cfgid', [
-        'c7f94f7d-1f57-4644-86d6-7004a2506680',
-    ])
-def test_conf1(cfgid):
-    drp = fridadrp.loader.drp_load()
+@pytest.mark.parametrize("conf, uuix", [
+    ['default', 'c7f94f7d-1f57-4644-86d6-7004a2506680'],
+    ['c7f94f7d-1f57-4644-86d6-7004a2506680', 'c7f94f7d-1f57-4644-86d6-7004a2506680'],
+])
+def test_loader1(conf, uuix):
+    import numina.core
+    from numina.instrument.assembly import assembly_instrument
 
-    assert cfgid in drp.configurations
+    obs = numina.core.ObservationResult(instrument='MEGARA')
+    obs.frames.append(create_simple_frame())
+    drpm = drp_load()
+    obs.configuration = conf
 
-    cfg = drp.configurations[cfgid]
-    assert isinstance(cfg, numina.core.pipeline.InstrumentConfiguration)
-    assert cfg.instrument == 'FRIDA'
-    assert cfg.uuid == cfgid
+    key, date_obs, keyname = drpm.select_profile(obs)
+    ins = assembly_instrument(drpm.configurations, key, date_obs, by_key=keyname)
+    assert isinstance(ins, numina.instrument.generic.InstrumentGeneric)
+    assert str(ins.origin.uuid) == uuix
 
