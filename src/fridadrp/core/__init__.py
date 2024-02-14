e1dbeccb6ff8bb5b5ef2033ab512e66a2037555d
Nicolás Cardiel <cardiel@ucm.es>
2024-02-14 18:54:36 +0100

    Define basic parameters: detector size, number of slices

---
+++ src/fridadrp/core/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright 2024 Universidad Complutense de Madrid
+#
+# This file is part of FRIDA DRP
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+FRIDA_NAXIS1_HAWAII = 2048  # dispersion direction
+FRIDA_NAXIS2_HAWAII = 2048  # spatial direction (slices)
+
+FRIDA_NAXIS1_IFU = 64  # parallel to the slices
+FRIDA_NAXIS2_IFU = 60  # perpendicular to the slices
+
+FRIDA_NSLICES = 30
