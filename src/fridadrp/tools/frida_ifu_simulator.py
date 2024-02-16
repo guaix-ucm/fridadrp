7a2aa515fa8456964e85908102c33dde6fbf81d1
Nicolás Cardiel <cardiel@ucm.es>
2024-02-16 10:17:37 +0100

    Redistribute code in frida_ifu_simulator and ifu_simulator

--- src/fridadrp/tools/frida_ifu_simulator.py
+++ src/fridadrp/tools/frida_ifu_simulator.py
@@ -8,10 +8,10 @@
 #
 
 import argparse
-from astropy.io import fits
-import matplotlib.pyplot as plt
 import pooch
 import sys
+
+from .ifu_simulator import ifu_simulator
 
 from fridadrp._version import version
 
@@ -23,12 +23,17 @@ from fridadrp.core import FRIDA_NAXIS2_IFU
 from fridadrp.core import FRIDA_NSLICES
 
 
-def download_auxiliary_images():
+def download_auxiliary_images(grating):
     """"Download auxiliary files when necessary
 
     # Note: compute md5 hash from terminal using:
     # linux $ md5sum <filename>
     # macOS $ md5 <filename>
+
+    Parameters
+    ----------
+    grating : str
+        Grating name.
 
     Returns
     -------
@@ -36,77 +41,40 @@ def download_auxiliary_images():
         File names of auxiliary files:
         - skycalc: table with SKYCALC Sky Model Calculator predictions
         - flatpix2pix: pixel-to-pixel flat field
-        - model_ifu2hawaii_medium-K: 2D polynomial transformation
+        - model_ifu2detector: 2D polynomial transformation
+          x_ifu, y_ify, wavelength -> x_detector, y_detector
 
     """
 
     faux_dict = {}
 
-    # SKYCALC Sky Model Calculator prediction table
-    faux_skycalc = pooch.retrieve(
-        'http://nartex.fis.ucm.es/~ncl/fridadrp_simulator_data/skycalc_R300000_table.fits',
-        known_hash='md5:49df0de4fc935de130eceacf5771350c',
-        progressbar=True
+    pooch_inst = pooch.create(
+        # use the default cache folder for the operating system
+        path=pooch.os_cache(project="fridadrp"),
+        # base URL for the remote data source
+        base_url='http://nartex.fis.ucm.es/~ncl/fridadrp_simulator_data/',
+        # specify the files that can be fetched
+        registry={
+            'skycalc_R300000_table.fits': 'md5:49df0de4fc935de130eceacf5771350c',
+            'simulated_flat_pix2pix.fits': 'md5:327b983843897df229cee42513912631',
+            'model_IFU2HAWAII_medium-K.json': 'md5:a708f8cf3f94e12f53a9833b853c2a3c'
+        }
     )
+
+    # SKYCALC Sky Model Calculator prediction table
+    faux_skycalc = pooch_inst.fetch('skycalc_R300000_table.fits', progressbar=True)
     faux_dict['skycalc'] = faux_skycalc
 
     # pixel-to-pixel flat field
-    faux_flatpix2pix = pooch.retrieve(
-        'http://nartex.fis.ucm.es/~ncl/fridadrp_simulator_data/simulated_flat_pix2pix.fits',
-        known_hash='md5:327b983843897df229cee42513912631',
-        progressbar=True
-    )
+    faux_flatpix2pix = pooch_inst.fetch('simulated_flat_pix2pix.fits', progressbar=True)
     faux_dict['flatpix2pix'] = faux_flatpix2pix
 
     # 2D polynomial transformation from IFU (x_ifu, y_ifu, wavelength) to
     # Hawaii coordinates (x_hawaii, y_hawaii)
-    faux_model_ifu2hawaii_medium_K = pooch.retrieve(
-        'http://nartex.fis.ucm.es/~ncl/fridadrp_simulator_data/model_IFU2HAWAII_medium-K.json',
-        known_hash='md5:a708f8cf3f94e12f53a9833b853c2a3c',
-        progressbar=True
-    )
-    faux_dict['model_ifu2hawaii_medium-K'] = faux_model_ifu2hawaii_medium_K
+    faux_model_ifu2detector = pooch_inst.fetch(f'model_IFU2HAWAII_{grating}.json', progressbar=True)
+    faux_dict['model_ifu2detector'] = faux_model_ifu2detector
 
     return faux_dict
-
-
-def display_skycalc(faux_skycalc):
-    """
-    Display sky radiance and transmission.
-
-    Data generated with the SKYCALC Sky Model Calculator tool
-    provided by ESO.
-    See https://www.eso.org/observing/etc/doc/skycalc/helpskycalc.html
-
-    Parameters
-    ----------
-    faux_skycalc : str
-        FITS file name with SKYCALC predictions.
-    """
-
-    with fits.open(faux_skycalc) as hdul:
-        skycalc_table = hdul[1].data
-    wave = skycalc_table['lam']
-    flux = skycalc_table['flux']
-    trans = skycalc_table['trans']
-
-    # plot radiance
-    fig, ax = plt.subplots()
-    ax.plot(wave, flux, '-', linewidth=1)
-    ax.set_xlabel('Vacuum Wavelength (nm)')
-    ax.set_ylabel('ph/s/m2/micron/arcsec2')
-    ax.set_title('SKYCALC prediction')
-    plt.tight_layout()
-    plt.show()
-
-    # plot transmission
-    fig, ax = plt.subplots()
-    ax.plot(wave, trans, '-', linewidth=1)
-    ax.set_xlabel('Vacuum Wavelength (nm)')
-    ax.set_ylabel('Transmission fraction')
-    ax.set_title('SKYCALC prediction')
-    plt.tight_layout()
-    plt.show()
 
 
 def main(args=None):
@@ -138,29 +106,20 @@ def main(args=None):
     if args.echo:
         print('\033[1m\033[31m% ' + ' '.join(sys.argv) + '\033[0m\n')
 
-    # Download auxiliary files when necessary
-    faux_dict = download_auxiliary_images()
-
-    # check grating calibration is available
     grating = args.grating
-    if f'model_ifu2hawaii_{grating}' in faux_dict:
-        print(f'Grating: {grating}')
-    else:
-        raise SystemExit(f'ERROR: calibration for grating "{grating}" not available yet!')
+
+    # Download auxiliary files when necessary
+    faux_dict = download_auxiliary_images(grating)
 
     rnoise = args.rnoise
     if rnoise < 0:
         raise ValueError(f'Invalid readout noise value: {rnoise}')
 
     verbose = args.verbose
-
-    if verbose:
-        for item in faux_dict:
-            print(f'- Required file: {faux_dict[item]}')
-
-    # display SKYCALC predictions for sky radiance and transmission
-    if verbose:
-        display_skycalc(faux_dict['skycalc'])
+    ifu_simulator(
+        faux_dict=faux_dict,
+        verbose=verbose
+    )
 
 
 if __name__ == "__main__":
---
+++ src/fridadrp/tools/ifu_simulator.py
@@ -0,0 +1,75 @@
+#
+# Copyright 2024 Universidad Complutense de Madrid
+#
+# This file is part of FRIDA DRP
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+
+def display_skycalc(faux_skycalc):
+    """
+    Display sky radiance and transmission.
+
+    Data generated with the SKYCALC Sky Model Calculator tool
+    provided by ESO.
+    See https://www.eso.org/observing/etc/doc/skycalc/helpskycalc.html
+
+    Parameters
+    ----------
+    faux_skycalc : str
+        FITS file name with SKYCALC predictions.
+    """
+
+    with fits.open(faux_skycalc) as hdul:
+        skycalc_table = hdul[1].data
+    wave = skycalc_table['lam']
+    flux = skycalc_table['flux']
+    trans = skycalc_table['trans']
+
+    # plot radiance
+    fig, ax = plt.subplots()
+    ax.plot(wave, flux, '-', linewidth=1)
+    ax.set_xlabel('Vacuum Wavelength (nm)')
+    ax.set_ylabel('ph/s/m2/micron/arcsec2')
+    ax.set_title('SKYCALC prediction')
+    plt.tight_layout()
+    plt.show()
+
+    # plot transmission
+    fig, ax = plt.subplots()
+    ax.plot(wave, trans, '-', linewidth=1)
+    ax.set_xlabel('Vacuum Wavelength (nm)')
+    ax.set_ylabel('Transmission fraction')
+    ax.set_title('SKYCALC prediction')
+    plt.tight_layout()
+    plt.show()
+
+
+def ifu_simulator(faux_dict, verbose):
+    """IFU simulator.
+
+    Parameters
+    ----------
+    faux_dict : Python dictionary
+        File names of auxiliary files:
+        - skycalc: table with SKYCALC Sky Model Calculator predictions
+        - flatpix2pix: pixel-to-pixel flat field
+        - model_ifu2detector: 2D polynomial transformation
+          x_ifu, y_ify, wavelength -> x_detector, y_detector
+    verbose : bool
+        If True, display/plot additional information.
+
+    Returns
+    -------
+    """
+
+    if verbose:
+        for item in faux_dict:
+            print(f'- Required file for item {item}:\n  {faux_dict[item]}')
+        # display SKYCALC predictions for sky radiance and transmission
+        display_skycalc(faux_dict['skycalc'])
