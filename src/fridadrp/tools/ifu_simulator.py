92ec1f102cc1351402f8cfac89a542d7daf98e47
Nicolás Cardiel <cardiel@ucm.es>
2024-03-15 09:03:01 +0100

    Define function generate_spectrum to modularize the code

--- src/fridadrp/tools/ifu_simulator.py
+++ src/fridadrp/tools/ifu_simulator.py
@@ -953,14 +953,14 @@ def save_image2d_rss_detector_method0(
 
 
 def set_wavelength_unit_and_range(scene_fname, scene_block, wmin, wmax, verbose):
-    """Set the wavelength unit and range
+    """Set the wavelength unit and range for a scene block.
 
     Parameters
     ----------
     scene_fname : str
         YAML scene file name.
     scene_block : dict
-        Dictonary storing a scene block.
+        Dictonary storing the scene block.
     wmin : `~astropy.units.Quantity`
         Minimum wavelength covered by the detector.
     wmax : `~astropy.units.Quantity`
@@ -970,6 +970,13 @@ def set_wavelength_unit_and_range(scene_fname, scene_block, wmin, wmax, verbose)
 
     Returns
     -------
+    wave_unit : `~astropy.units.core.Unit`
+        Wavelength unit to be used in the scene block.
+    wave_min : `~astropy.units.Quantity`
+        Minimum wavelength to be used in the scene block.
+    wave_max : `~astropy.units.Quantity`
+        Maximum wavelength to be used in the scene block.
+
     """
 
     expected_keys_in_spectrum = {'type', 'wave_unit'}
@@ -1008,6 +1015,172 @@ def set_wavelength_unit_and_range(scene_fname, scene_block, wmin, wmax, verbose)
             wave_max *= Unit(wave_unit)
 
         return wave_unit, wave_min, wave_max
+
+
+def generate_spectrum(scene_fname, scene_block, faux_dict, wave_unit,
+                      wave_min, wave_max, nphotons,
+                      atmosphere_transmission, wave_transmission, curve_transmission,
+                      rng, naxis1_detector,
+                      verbose, plots):
+    """Generate photons for the scene block.
+
+    Parameters
+    ----------
+    scene_fname : str
+        YAML scene file name.
+    scene_block : dict
+        Dictonary storing a scene block.
+    faux_dict : Python dictionary
+        File names of auxiliary files:
+        - skycalc: table with SKYCALC Sky Model Calculator predictions
+        - flatpix2pix: pixel-to-pixel flat field
+        - model_ifu2detector: 2D polynomial transformation
+          x_ifu, y_ify, wavelength -> x_detector, y_detector
+    wave_unit : `~astropy.units.core.Unit`
+        Wavelength unit to be used in the scene block.
+    wave_min : `~astropy.units.Quantity`
+        Minimum wavelength to be used in the scene block.
+    wave_max : `~astropy.units.Quantity`
+        Maximum wavelength to be used in the scene block.
+    nphotons : int
+        Number of photons to be generated in the scene block.
+    rng : `~numpy.random._generator.Generator`
+        Random number generator.
+    naxis1_detector : `~astropy.units.Quantity`
+        Detector NAXIS1, dispersion direction.
+    verbose : bool
+        If True, display additional information.
+    plots : bool
+        If True, plot intermediate results.
+
+    Returns
+    -------
+    simulated_wave : '~numpy.ndarray'
+        Array containint `nphotons` simulated photons with the
+        spectrum requested in the scene block.
+
+    """
+
+    spectrum_type = scene_block['spectrum']['type']
+    if spectrum_type == 'delta-lines':
+        filename = scene_block['spectrum']['filename']
+        wave_column = scene_block['spectrum']['wave_column'] - 1
+        flux_column = scene_block['spectrum']['flux_column'] - 1
+        if filename[0] == '@':
+            # retrieve file name from dictionary of auxiliary
+            # file names for the considered instrument
+            filename = faux_dict[filename[1:]]
+        catlines = np.genfromtxt(filename)
+        line_wave = catlines[:, wave_column] * Unit(wave_unit)
+        if not np.all(np.diff(line_wave.value) > 0):
+            raise_ValueError(f'Wavelength array {line_wave=} is not sorted!')
+        line_flux = catlines[:, flux_column]
+        simulated_wave = simulate_delta_lines(
+            line_wave=line_wave,
+            line_flux=line_flux,
+            nphotons=nphotons,
+            rng=rng,
+            wmin=wave_min,
+            wmax=wave_max,
+            plots=plots,
+            plot_title=filename
+        )
+        if atmosphere_transmission == "default":
+            if verbose:
+                print('No atmosphere transmission applied to this type of data')
+    elif spectrum_type == 'skycalc-radiance':
+        faux_skycalc = faux_dict['skycalc']
+        with fits.open(faux_skycalc) as hdul:
+            skycalc_table = hdul[1].data
+        wave = skycalc_table['lam'] * Unit(wave_unit)
+        if not np.all(np.diff(wave.value) > 0):
+            raise_ValueError(f'Wavelength array {wave=} is not sorted!')
+        flux = skycalc_table['flux']
+        flux_type = 'photlam'
+        simulated_wave = simulate_spectrum(
+            wave=wave,
+            flux=flux,
+            flux_type=flux_type,
+            nphotons=nphotons,
+            rng=rng,
+            wmin=wave_min,
+            wmax=wave_max,
+            convolve_sigma_km_s=0 * u.km / u.s,
+            nbins_histo=naxis1_detector.value,
+            plots=plots,
+            plot_title=os.path.basename(faux_skycalc),
+            verbose=verbose
+        )
+        if atmosphere_transmission == "default":
+            print('No atmosphere transmission applied to this type of data')
+    elif spectrum_type == 'tabulated-spectrum':
+        filename = scene_block['spectrum']['filename']
+        wave_column = scene_block['spectrum']['wave_column'] - 1
+        flux_column = scene_block['spectrum']['flux_column'] - 1
+        flux_type = scene_block['spectrum']['flux_type']
+        if 'redshift' in scene_block['spectrum']:
+            redshift = scene_block['spectrum']['redshift']
+        else:
+            if verbose:
+                print(ctext('Assuming redshift: 0', faint=True))
+            redshift = 0.0
+        if 'convolve_sigma_km_s' in scene_block['spectrum']:
+            convolve_sigma_km_s = scene_block['spectrum']['convolve_sigma_km_s']
+        else:
+            if verbose:
+                print(ctext('Assuming convolve_sigma_km_s: 0', faint=True))
+            convolve_sigma_km_s = 0.0
+        convolve_sigma_km_s *= u.km / u.s
+        # read data
+        table_data = np.genfromtxt(filename)
+        wave = table_data[:, wave_column] * (1 + redshift) * Unit(wave_unit)
+        if not np.all(np.diff(wave.value) > 0):
+            raise_ValueError(f'Wavelength array {wave=} is not sorted!')
+        flux = table_data[:, flux_column]
+        simulated_wave = simulate_spectrum(
+            wave=wave,
+            flux=flux,
+            flux_type=flux_type,
+            nphotons=nphotons,
+            rng=rng,
+            wmin=wave_min,
+            wmax=wave_max,
+            convolve_sigma_km_s=convolve_sigma_km_s,
+            nbins_histo=naxis1_detector.value,
+            plots=plots,
+            plot_title=os.path.basename(filename),
+            verbose=verbose
+        )
+        if atmosphere_transmission == "default":
+            apply_atmosphere_transmission(
+                simulated_wave=simulated_wave,
+                wave_transmission=wave_transmission,
+                curve_transmission=curve_transmission,
+                rng=rng,
+                verbose=verbose,
+                plots=plots
+            )
+    elif spectrum_type == 'constant-flux':
+        simulated_wave = simulate_constant_photlam(
+            wmin=wave_min,
+            wmax=wave_max,
+            nphotons=nphotons,
+            rng=rng
+        )
+        if atmosphere_transmission == "default":
+            apply_atmosphere_transmission(
+                simulated_wave=simulated_wave,
+                wave_transmission=wave_transmission,
+                curve_transmission=curve_transmission,
+                rng=rng,
+                verbose=verbose,
+                plots=plots
+            )
+    else:
+        simulated_wave = None  # avoid PyCharm warning (not aware of raise ValueError)
+        raise_ValueError(f'Unexpected {spectrum_type=} in file {scene_fname}')
+
+    return simulated_wave
 
 
 def ifu_simulator(wcs3d, naxis1_detector, naxis2_detector, nslices,
@@ -1164,123 +1337,22 @@ def ifu_simulator(wcs3d, naxis1_detector, naxis2_detector, nslices,
                 # -------------------------------------------------------------
                 # spectrum type
                 # -------------------------------------------------------------
-                spectrum_type = scene_block['spectrum']['type']
-                if spectrum_type == 'delta-lines':
-                    filename = scene_block['spectrum']['filename']
-                    wave_column = scene_block['spectrum']['wave_column'] - 1
-                    flux_column = scene_block['spectrum']['flux_column'] - 1
-                    if filename[0] == '@':
-                        # retrieve file name from dictionary of auxiliary
-                        # file names for the considered instrument
-                        filename = faux_dict[filename[1:]]
-                    catlines = np.genfromtxt(filename)
-                    line_wave = catlines[:, wave_column] * Unit(wave_unit)
-                    if not np.all(np.diff(line_wave.value) > 0):
-                        raise_ValueError(f'Wavelength array {line_wave=} is not sorted!')
-                    line_flux = catlines[:, flux_column]
-                    simulated_wave = simulate_delta_lines(
-                        line_wave=line_wave,
-                        line_flux=line_flux,
-                        nphotons=nphotons,
-                        rng=rng,
-                        wmin=wave_min,
-                        wmax=wave_max,
-                        plots=plots,
-                        plot_title=filename
-                    )
-                    if atmosphere_transmission == "default":
-                        if verbose:
-                            print('No atmosphere transmission applied to this type of data')
-                elif spectrum_type == 'skycalc-radiance':
-                    faux_skycalc = faux_dict['skycalc']
-                    with fits.open(faux_skycalc) as hdul:
-                        skycalc_table = hdul[1].data
-                    wave = skycalc_table['lam'] * Unit(wave_unit)
-                    if not np.all(np.diff(wave.value) > 0):
-                        raise_ValueError(f'Wavelength array {wave=} is not sorted!')
-                    flux = skycalc_table['flux']
-                    flux_type = 'photlam'
-                    simulated_wave = simulate_spectrum(
-                        wave=wave,
-                        flux=flux,
-                        flux_type=flux_type,
-                        nphotons=nphotons,
-                        rng=rng,
-                        wmin=wave_min,
-                        wmax=wave_max,
-                        convolve_sigma_km_s=0*u.km/u.s,
-                        nbins_histo=naxis1_detector.value,
-                        plots=plots,
-                        plot_title=os.path.basename(faux_skycalc),
-                        verbose=verbose
-                    )
-                    if atmosphere_transmission == "default":
-                        print('No atmosphere transmission applied to this type of data')
-                elif spectrum_type == 'tabulated-spectrum':
-                    filename = scene_block['spectrum']['filename']
-                    wave_column = scene_block['spectrum']['wave_column'] - 1
-                    flux_column = scene_block['spectrum']['flux_column'] - 1
-                    flux_type = scene_block['spectrum']['flux_type']
-                    if 'redshift' in scene_block['spectrum']:
-                        redshift = scene_block['spectrum']['redshift']
-                    else:
-                        if verbose:
-                            print(ctext('Assuming redshift: 0', faint=True))
-                        redshift = 0.0
-                    if 'convolve_sigma_km_s' in scene_block['spectrum']:
-                        convolve_sigma_km_s = scene_block['spectrum']['convolve_sigma_km_s']
-                    else:
-                        if verbose:
-                            print(ctext('Assuming convolve_sigma_km_s: 0', faint=True))
-                        convolve_sigma_km_s = 0.0
-                    convolve_sigma_km_s *= u.km / u.s
-                    # read data
-                    table_data = np.genfromtxt(filename)
-                    wave = table_data[:, wave_column] * (1 + redshift) * Unit(wave_unit)
-                    if not np.all(np.diff(wave.value) > 0):
-                        raise_ValueError(f'Wavelength array {wave=} is not sorted!')
-                    flux = table_data[:, flux_column]
-                    simulated_wave = simulate_spectrum(
-                        wave=wave,
-                        flux=flux,
-                        flux_type=flux_type,
-                        nphotons=nphotons,
-                        rng=rng,
-                        wmin=wave_min,
-                        wmax=wave_max,
-                        convolve_sigma_km_s=convolve_sigma_km_s,
-                        nbins_histo=naxis1_detector.value,
-                        plots=plots,
-                        plot_title=os.path.basename(filename),
-                        verbose=verbose
-                    )
-                    if atmosphere_transmission == "default":
-                        apply_atmosphere_transmission(
-                            simulated_wave=simulated_wave,
-                            wave_transmission=wave_transmission,
-                            curve_transmission=curve_transmission,
-                            rng=rng,
-                            verbose=verbose,
-                            plots=plots
-                        )
-                elif spectrum_type == 'constant-flux':
-                    simulated_wave = simulate_constant_photlam(
-                        wmin=wave_min,
-                        wmax=wave_max,
-                        nphotons=nphotons,
-                        rng=rng
-                    )
-                    if atmosphere_transmission == "default":
-                        apply_atmosphere_transmission(
-                            simulated_wave=simulated_wave,
-                            wave_transmission=wave_transmission,
-                            curve_transmission=curve_transmission,
-                            rng=rng,
-                            verbose=verbose,
-                            plots=plots
-                        )
-                else:
-                    raise_ValueError(f'Unexpected {spectrum_type=} in file {scene_fname}')
+                simulated_wave = generate_spectrum(
+                    scene_fname=scene_fname,
+                    scene_block=scene_block,
+                    faux_dict=faux_dict,
+                    wave_unit=wave_unit,
+                    wave_min=wave_min,
+                    wave_max=wave_max,
+                    nphotons=nphotons,
+                    atmosphere_transmission=atmosphere_transmission,
+                    wave_transmission=wave_transmission,
+                    curve_transmission=curve_transmission,
+                    rng=rng,
+                    naxis1_detector=naxis1_detector,
+                    verbose=verbose,
+                    plots=plots
+                )
                 # convert to default wavelength_unit
                 simulated_wave = simulated_wave.to(wv_cunit1)
                 if nphotons_all == 0:
