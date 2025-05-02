0889b9e9f38dae2287c9ffa4dd382478ab3ccaca
Nicolás Cardiel <cardiel@ucm.es>
2025-05-02 12:52:54 +0200

    Fill __init__.py corresponding to fridadrp package

---
+++ src/fridadrp/__init__.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2025 Universidad Complutense de Madrid
+#
+# This file is part of FRIDA DRP
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+"""The MEGARA Data Reduction Pipeline"""
+
+import logging
+
+from fridadrp._version import __version__  # noqa: F401
+
+
+# Top level NullHandler
+logging.getLogger("fridadrp").addHandler(logging.NullHandler())
